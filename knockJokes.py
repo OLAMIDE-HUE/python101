42925872f46977265db961c9b0aac6627bbe5c28
OLAMIDE-HUE <oogunjobi5698@students.mchenry.edu>
2025-03-01 19:21:07 -0600

    removed

--- knockJokes.py
+++
@@ -1,6 +0,0 @@
-# prints a knock knock joke
-<<<<<<< HEAD
-print("Knock, knock\nWho's there?\nLettuce\nLettuce who?\nLettuce in, it's cold out here.")
-=======
-print("Knock, knock\nWho's there?\nLettuce\nLettuce who?\nLettuce in, it's cold out here.")
->>>>>>> 34b9ba8a79db5afd5e6811a8530571a7d1ca3f31
