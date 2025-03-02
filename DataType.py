5f5bfa56558e19555d6c8fc83b7b4e951f132351
OLAMIDE-HUE <oogunjobi5698@students.mchenry.edu>
2025-03-02 16:17:21 -0600

    learning data points

---
+++ DataType.py
@@ -0,0 +1,21 @@
+
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 10)
+
+# Integer
+y = -34
+print("Integer:", y)
+
+# Float
+z = 24.9
+print("Float:", z)
+
+# String
+name = "Mark"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
