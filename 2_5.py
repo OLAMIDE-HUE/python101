f15a2d7f6b66a72e98e5b53621b10268da97b371
OLAMIDE-HUE <oogunjobi5698@students.mchenry.edu>
2025-03-02 16:33:18 -0600

    practicing comments

---
+++ 2_5.py
@@ -0,0 +1,14 @@
+# Sets the value of tank_size to be 15 gallons
+tank_size = 15
+
+# Define the total distance covered
+# Sets the value of distance to be 600 miles
+distance = 600
+
+# Calculate miles per gallon
+# Divides the distance in miles by the tank_size in gallons
+mpg = distance / tank_size
+
+# Print the calculated miles per gallon
+# Prints using multiple arguments
+print("Miles per gallon:", mpg)
