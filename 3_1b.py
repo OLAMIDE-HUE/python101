302845ebf8815cb9bf784c62da8037db02875aae
OLAMIDE-HUE <oogunjobi5698@students.mchenry.edu>
2025-03-03 21:07:30 -0600

    changes numerical grade to letter grade

---
+++ 3_1b.py
@@ -0,0 +1,14 @@
+grade = int(input("Please enter in a numerical grade between 1 and 100 inclusively: "))
+
+if (grade < 0 or grade > 100):
+    print("Please enter in a numerical grade between 1 and 100 inclusively: ")
+elif (grade >= 90):
+    print(f"The letter grade is: A")
+elif(grade >= 80):
+    print(f"The letter grade is: B")
+elif(grade >= 70):
+    print(f"The letter grade is: C")
+elif(grade >= 60):
+    print(f"The letter grade is: D")
+else:
+    print(f"The letter grade is: F")
