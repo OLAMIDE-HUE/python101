1cfa0195393602097fb59e46eba9c221bd63727d
OLAMIDE-HUE <oogunjobi5698@students.mchenry.edu>
2025-03-03 22:20:31 -0600

    using while loop to print a song

--- 3_2a.py
+++ 3_2a.py
@@ -1,5 +1,7 @@
+# variable to hold the initial number of beers
 num = 99
 
+# loops through each decrement from 99 till there are no bottles left
 while (num != 1):
     print(f"{num} bottles of beer on the wall\n{num} bottles of beer\nTake one down, pass it around")
     print(f"{num-1} bottles of bear on the wall!\n")
