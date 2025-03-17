a038f25b3eaf4bd227105e1ca72961a92309cf80
OLAMIDE-HUE <oogunjobi5698@students.mchenry.edu>
2025-03-17 10:11:33 -0500

    defining and calling a function

---
+++ 4.1.py
@@ -0,0 +1,13 @@
+# defining the square function
+def square(side):
+    result = side * side
+    print(f"The area of the square is {result} square units.")
+
+#defining the circle function
+def circle(radius):
+    result = 3.14 * radius * radius
+    print(f"The area of the circle is {result} square units.")
+
+# calling the functions
+square(4)
+circle(5)
