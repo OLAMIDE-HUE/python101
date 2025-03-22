23c25e0da469b181d67ad786a2091a192ce12a5b
OLAMIDE-HUE <oogunjobi5698@students.mchenry.edu>
2025-03-22 16:19:08 -0500

    using recursion to calculate factorial

--- 4.5.py
+++ 4.5.py
@@ -1,14 +1,16 @@
 # defining the factorial function
 def factorial(n):
-    if n < 0:
-        return None
-    if n < 2:
+    if n == 0 or n == 1:
         return 1
     return n * factorial(n-1)
 
 
 # getting user input
-num = int(input("Enter a nonnegative number: "))
+num = int(input("Please enter a nonnegative number: "))
+
+# ensures the user enters zero or a positive number
+while (num < 0):
+    num = int(input("Please enter a nonnegative number: "))
 
 # printing factorial of user's number
 print(factorial(num))
