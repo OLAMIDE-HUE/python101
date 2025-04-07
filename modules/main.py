00b8affd51d3090cd29da95dc9c51094f042e684
OLAMIDE-HUE <oogunjobi5698@students.mchenry.edu>
2025-04-07 12:38:42 -0500

    using modules

---
+++ modules/main.py
@@ -0,0 +1,7 @@
+from math_operations import calculator, geometry
+
+result1 = calculator.add(7, 2)
+print("Result1:", result1)
+
+result2 = geometry.rectangle_area(2, 4)
+print("Result2:", result2)
---
+++ modules/math_operations/geometry.py
@@ -0,0 +1,15 @@
+# area of a square
+def square_area(a):
+    return a * a
+
+# area of a rectangle
+
+
+def rectangle_area(a, b):
+    return a * b
+
+# area of a triangle
+
+
+def triangle_area(b, h):
+    return 0.5 * b * h
