9833108e79bc8486d0aa0a334a43db7bb1ac74e0
OLAMIDE-HUE <oogunjobi5698@students.mchenry.edu>
2025-04-12 20:13:55 -0500

    replace index in array with exception

--- (2)2.7,8.py
+++ (2)2.7,8.py
@@ -3,17 +3,26 @@ def main():
                    "Stevie Wonder", "Janet Jackson", "Michael Jackson", "Whitney Houston", "Rihanna"]
     # Your code goes here
     try:
+
+        # gets the user's index
         users_index = int(
             input("Please enter what index you would like to replace: "))
+
+        # gets the user's new artist
         users_artist = input("Please enter a new artists name: ")
 
+        # replaces the chosen index with the new artist
         top_artists[users_index] = users_artist
 
+        # prints the new array
         print(top_artists)
+
+    # runs if the user does not enter an integer for the index or enters an invalid index
     except (ValueError, IndexError):
         print("An input error occurred.")
     except:
         print("There is an error!!!")
 
 
+# calling the main function
 main()
