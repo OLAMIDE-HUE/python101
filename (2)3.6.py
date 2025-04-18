2400a8c06e24de5ee86fe165d1302bf480ec2518
OLAMIDE-HUE <oogunjobi5698@students.mchenry.edu>
2025-04-18 09:29:35 -0500

    custom exception

---
+++ (2)3.6.py
@@ -0,0 +1,35 @@
+# subclass of base exception class
+class NotNumericError(Exception):
+
+    # constructor
+    def __init__(self, num, message):
+        super().__init__(message)
+        self.number = num
+
+# defining the main function
+
+
+def main():
+    done = False
+
+    while not done:
+        try:
+            # prompts the user for an input
+            num = input("Enter a number: ")
+
+            if not num.isnumeric():
+                raise NotNumericError(num, "Not a number")
+
+        except NotNumericError as n:
+            print(n, ": ", n.number)
+
+        else:
+            done = True
+            print("You entered a valid number!")
+
+        finally:
+            print("This is the end of the program!")
+
+
+# calling the main function
+main()
